76ab0124c7decc5b74c3ad5751e1aed64394fde2
99003776 <78869826+99003776@users.noreply.github.com>
2021-04-12 00:26:33 +0530

    upload folder

---
+++ Activity_Task/IOCTL_traverse/user.c
@@ -0,0 +1,72 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include<linux/ioctl.h>
+#include<fcntl.h>
+
+#define IOC_MAGIC 'p'
+#define MY_IOCTL_RESET  _IO(IOC_MAGIC, 3)
+#define MY_IOCTL_AVAIL  _IO(IOC_MAGIC, 2)
+#define MY_IOCTL_LEN	_IO(IOC_MAGIC, 1)
+#define MY_MACIG ']'
+#define READ_IOCTL _IOR(MY_MACIG, 0, int)
+#define WRITE_IOCTL _IOW(MY_MACIG, 1, int)
+
+void printdata(void);
+
+int main()
+{
+int nbytes,ret;
+ printf("hello;worldsd");
+int fd=open("/dev/psample0",O_RDWR);
+printf("\n%d\n",fd);
+if(fd<0)
+	perror("open");
+char str[]="worldhello";
+ret=ioctl(fd, MY_IOCTL_LEN);
+if(ret<0)
+{
+   perror("ioctl");
+   exit(3);
+}
+ret=ioctl(fd, MY_IOCTL_AVAIL);
+if(ret<0)
+{
+  perror("ioctl");
+  exit(3);
+}
+
+ret=ioctl(fd, MY_IOCTL_AVAIL);
+if(ret<0)
+{
+  perror("ioctl");
+  exit(3);
+}
+
+ret=ioctl(fd, MY_IOCTL_RESET);
+if(ret<0) 
+{
+  perror("ioctl");
+  exit(3);
+}
+
+ret=ioctl(fd,WRITE_IOCTL,123);
+if(ret<0)
+{
+  perror("ioctl");
+  exit(3);
+}
+
+unsigned long abc;
+ret=ioctl(fd,READ_IOCTL);
+if(ret<0)
+{
+  perror("ioctl");
+  exit(3);
+}
+
+if(nbytes<0)
+{
+  perror("read");
+}
+close(fd);
+}
